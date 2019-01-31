fcaa7c885ee54200c10de6d812864e9dac400e1a
Roberto Cantore <rcantore@gmail.com>
2019-01-30 21:40:28 -0300

    added new configs

--- macos/macosdefaults.sh
+++ macos/macosdefaults.sh
@@ -4,3 +4,5 @@ defaults write com.apple.screencapture location -string “$HOME/Pictures
 defaults write com.apple.screencapture type -string “png”
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# Accelarate Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
